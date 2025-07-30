b8d4dae42299df0a68ff7078ebfadb5fe98c7b99
Permata-16 <putrinabbilla4@gmail.com>
2025-07-30 12:25:47 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'bioskop',
+  webDir: 'www'
+};
+
+export default config;
